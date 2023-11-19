c4fe7f9fe8799583fbd37abf021b6b4db18b7a19
Ayushi Shah <as17726@nyu.edu>
2023-11-19 17:15:17 -0500

    initial setup

--- HW_11.ino
+++ HW_11.ino
@@ -1,9 +1,17 @@
+// int pCount = 0; 
+int pOn = 1; 
+int pOff = 0; 
+int pState = off; 
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(6, OUTPUT); 
+  pinMode(7, OUTPUT); 
+  pinMode(0, INPUT); 
+  Serial.begin(9600); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # HW_11
  
+Drawing of the Finite State Machine for your password.
+Description of your logic and the correct steps necessary for a successful sequence.
+A schematic drawing of your circuit.
