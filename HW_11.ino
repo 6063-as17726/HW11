4c71d45f000036f407ab971f2b14429c160676ae
Ayushi Shah <as17726@nyu.edu>
2023-11-21 14:45:57 -0500

    wrote code

--- HW_11.ino
+++ HW_11.ino
@@ -1,17 +1,62 @@
+#include "Clicky.h"
+
 // int pCount = 0; 
-int pOn = 1; 
-int pOff = 0; 
-int pState = off; 
+int pRight = 1; 
+int pWrong = 0; 
+int pState = pRight; 
+
+Clicky pClick;
+
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(6, OUTPUT); 
   pinMode(7, OUTPUT); 
-  pinMode(0, INPUT); 
+  pinMode(8, OUTPUT); 
+  pinMode(4, INPUT); 
+  pinMode(2, INPUT); 
   Serial.begin(9600); 
+  pClick.setup(4);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+//put your main code here, to run repeatedly:
+    bool i0 = pClick.wasClicked();
+    int i1 = digitalRead(2); 
+    int i2 = analogRead(A6); 
+    int i3 = digitalRead(4); 
+
+  if(pState == pRight && i0 == 1) {
+    pState = pRight; 
+  } else {
+    pState = pWrong; 
+  }
   
+  if(pState == pRight && i1 == 1) {
+    pState = pRight; 
+  } else {
+    pState = pWrong; 
+  }
+
+  if (pState == pRight && i2 > 300) { 
+    pState = pRight;  
+  } else {
+    pState = pWrong; 
+  }
+
+  if(pState == pRight && i3 == 1) {
+    pState = pRight; 
+  } else {
+    pState = pWrong; 
+  }
+
+  if (pState == pRight) {
+    digitalWrite(7, HIGH); 
+  } else {
+    digitalWrite (8, HIGH); 
+  }
+
+  Serial.println(String(pState));
+
+  delay(100); 
+
 }
